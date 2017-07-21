204a27fb5368043d97223713b51f51b64d3eb58d
daniel0318 <dani_0318@hotmail.com>
2017-07-20 23:52:24 -0500

    Generador de niveles terminado
    Ya genera un nivel aleatoriamente cada vez que se ejecuta el juego.

---
+++ Rogue/Assets/Scripts/BoardManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour {
+
+	public int columns = 8;
+	public int rows = 8;
+	private Transform boardHolder;
+
+	public GameObject [] floorTiles, outerWallTiles; // Arrays donde estan los gameObjects de los pisos y las paredes externas.
+
+	public void SetupScene(){
+		
+		Debug.Log ("Se ejecuto!!");
+		BoardSetup ();
+	}
+
+	//Encargado de pintar el tablero
+	void BoardSetup(){
+
+		boardHolder = new GameObject ("board").transform;
+		for(int i = -1; i < columns +1 ;i++){
+			Debug.Log ("Entro");
+
+			for(int j = -1; j < rows +1 ;j++){
+				Debug.Log ("Entro2");
+
+				GameObject toInstantiate = GetRandomInArray (floorTiles);
+
+				if (i == -1 || j == -1 || i == columns || j == rows) {
+					toInstantiate = GetRandomInArray (outerWallTiles);
+				}
+			
+				GameObject instance = Instantiate (toInstantiate,new Vector2(i,j), Quaternion.identity);
+				instance.transform.SetParent (boardHolder);
+			}
+
+		}
+
+	}
+
+	GameObject GetRandomInArray(GameObject [] array){
+		Debug.Log ("pidio");
+		return array [Random.Range (0, array.Length)];
+	}
+}
