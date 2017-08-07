545f2e1eede83c175702c8f5849db2cd29eee199
daniel0318 <dani_0318@hotmail.com>
2017-08-06 22:49:49 -0500

    Script de destruir muro y animacion del jugador terminado
    El script de los los muros ya esta hecho y ya se coloco a cada prefab de los muros.
    La animacion del jugador ya se termino tanto para cuando golpea como cuando es golpeado.

---
+++ Rogue/Assets/Scripts/Wall.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour {
+	
+	public Sprite dmgSprite;
+	public int hp = 4;
+	private SpriteRenderer sprinteRenderer;
+
+
+	// Use this for initialization
+	private void Awake () {
+		
+		sprinteRenderer = GetComponent<SpriteRenderer>();
+
+	}
+
+	public void DamageWall(int loss){
+
+		sprinteRenderer.sprite = dmgSprite;
+		hp -= loss;
+		if(hp<0){
+			/*No es aconsejable destruir un objeto durante la partida
+			para que no se ejecute el recolector de basura y disminuya
+			el rendimiento del juego. Es mejor deshabilitarlo*/
+			//Destroy (gameObject);
+			gameObject.SetActive(false);
+		}
+	}
+}
